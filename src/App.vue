b539222f0014662676971e3ca311e36867403e0b
TE-aj <aj.page@techelevator.com>
2023-04-29 11:13:09 -0600

    uniform app styling

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,30 @@
       <img v-on:click="playMusic" alt="nutsy logo" class="logo" src="https://static.wikia.nocookie.net/finalfantasy/images/3/33/ClanNutsy.png" width="240" height="160" />
     </header>
   </div>
+
+  <div id="newsContainer">
+    <!-- <NewsBar/> -->
+  </div>
   
-  <RouterView class="view"/>
+  <div id="mainView">
+    <RouterView class="view"/>
+  </div>
   
-  <footer>
-    <img src="/microsoft-backoffice-1996.gif" alt="powered by ms backoffice">
-    <a href="https://www.gamefaqs.com">
-      <img src="/bar2001.gif" alt="gameFaqs">
-    </a>
-    <img src="/frontpage-1996.gif" alt="made with frontpage">
-    <audio id="music" src="https://audio.jukehost.co.uk/FUx1r78wxSbDlsaZ1KJajh2PZaBA46fK" type="audio/mpeg" loop></audio>
-    <img src="https://hitwebcounter.com/counter/counter.php?page=8523078&style=0040&nbdigits=5&type=page&initCount=0" Alt="web counter"/>
-    <img src="/mac-mall-1998.gif" alt="mac-mall-offer">
-    <img src="/clickconf.gif" height="48" alt="esrb" id="esrb" v-on:click="say('thank you for confirming your privacy')">
-  </footer>
+  <div id="bottom">
+
+    <footer>
+      <img src="/microsoft-backoffice-1996.gif" alt="powered by ms backoffice">
+      <a href="https://www.gamefaqs.com">
+        <img src="/bar2001.gif" alt="gameFaqs">
+      </a>
+      <img src="/frontpage-1996.gif" alt="made with frontpage">
+      <audio id="music" src="https://audio.jukehost.co.uk/FUx1r78wxSbDlsaZ1KJajh2PZaBA46fK" type="audio/mpeg" loop></audio>
+      <img src="https://hitwebcounter.com/counter/counter.php?page=8523078&style=0040&nbdigits=5&type=page&initCount=0" Alt="web counter"/>
+      <img src="/mac-mall-1998.gif" alt="mac-mall-offer">
+      <img src="/clickconf.gif" height="48" alt="esrb" id="esrb" v-on:click="say('thank you for confirming your privacy')">
+    </footer>
+  </div>
+
 </template>
 
 <style>
@@ -33,6 +43,7 @@
 #topper{
   grid-area: topper;
   height: fit-content;
+  width: 100%;
 }
 
 .logo{
@@ -53,12 +64,21 @@ body {
   width: 100vw;
   display: grid;
   align-items: center;
+  justify-items: center;
   grid-template-columns: 1fr 2fr 1fr;
   grid-template-areas:
-  ". topper ."
-  ". main ."
-  ". footer .";
+  "newsBar topper ."
+  "newsBar main ."
+  "newsBar footer .";
 
+}
+
+#newsContainer{
+  /* border goes on internal component, not container */
+  /* border: 8px ridge lightgray; */
+  grid-area: newsBar;
+  width: 75%;
+  height: 75%;
 }
 
 header {
@@ -69,19 +89,32 @@ header {
   justify-content: center;
   border: 8px ridge lightgray;
 }
+#mainView {
+  grid-area: main;
+  width: 100%;
+
+}
 
 .view {
-  grid-area: main;
   border: 8px ridge lightgray;
   height: fit-content;
 }
 
 footer {
-  grid-area: footer;
   display: flex;
   align-items: center;
   border: 8px ridge lightgray;
   justify-content: center;
+  width: 100%;
+
+}
+
+#bottom {
+  grid-area: footer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 
 }
 
@@ -126,51 +159,45 @@ import { useAuthenticationStore } from './stores/AuthenticationStore'
 import NewsBar from './components/NewsBar.vue'
 
 export default {
-
-  setup(){
-    const authStore = useAuthenticationStore()
-    return {
-      authStore
-    }
-  },
-
-  data(){
+    setup() {
+        const authStore = useAuthenticationStore();
         return {
-          playing: false,
-        }
-  },
-
-mounted(){
-
-// let z = document.getElementsByTagName("main");
-// window.addEventListener('mousemove', (event)=> {
-//   let x = document.getElementById("music"); 
-//   x.play(); 
-// })
-
-},
-
-methods: {
-  say(message) {
-    alert(message)
-  },
-  playMusic(){
-    let x = document.getElementById("music"); 
-
-    if(this.playing){
-      x.pause();
-      this.playing = false;
-    } else {
-      x.play(); 
-      this.playing = true;
-    }
-  },
-  logOff(){
-    this.authStore.LOGOUT();
-    this.$router.push("/login");
-  },
-}
-
+            authStore
+        };
+    },
+    data() {
+        return {
+            playing: false,
+        };
+    },
+    mounted() {
+        // let z = document.getElementsByTagName("main");
+        // window.addEventListener('mousemove', (event)=> {
+        //   let x = document.getElementById("music"); 
+        //   x.play(); 
+        // })
+    },
+    methods: {
+        say(message) {
+            alert(message);
+        },
+        playMusic() {
+            let x = document.getElementById("music");
+            if (this.playing) {
+                x.pause();
+                this.playing = false;
+            }
+            else {
+                x.play();
+                this.playing = true;
+            }
+        },
+        logOff() {
+            this.authStore.LOGOUT();
+            this.$router.push("/login");
+        },
+    },
+    components: { NewsBar }
 }
 
 </script>
